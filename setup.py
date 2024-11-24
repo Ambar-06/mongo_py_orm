6d391ae482e25aaf3fa6be6c30d8cbae77e0cb52
Ambar Rizvi <brannstrom9911@gmail.com>
2024-11-24 20:08:13 +0530

    fixed bson issue

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 DESCRIPTION = 'A lightweight Python Object-Relational Mapper (ORM)-like implementation for MongoDB'
 with open('README.md', 'r', encoding='utf-8') as f: 
     LONG_DESCRIPTION = f.read()
