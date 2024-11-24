bb06cd37a4dcf7a644c6a2be110b33e80569d74f
Ambar Rizvi <brannstrom9911@gmail.com>
2024-11-24 19:32:34 +0530

    version change

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'A lightweight Python Object-Relational Mapper (ORM)-like implementation for MongoDB'
 with open('README.md', 'r', encoding='utf-8') as f: 
     LONG_DESCRIPTION = f.read()
