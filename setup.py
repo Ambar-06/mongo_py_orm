04fb2ec8e9166af649d5428f7c6c0d0cbdc13cd7
Ambar Rizvi <brannstrom9911@gmail.com>
2024-12-13 22:00:20 +0530

    added len dunder method

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.2.8'
+VERSION = '0.2.9'
 DESCRIPTION = 'A lightweight Python Object-Relational Mapper (ORM)-like implementation for MongoDB'
 with open('README.md', 'r', encoding='utf-8') as f: 
     LONG_DESCRIPTION = f.read()
